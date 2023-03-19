983df9d7b6b90b7bb7cfba201d6397f7b18836eb
Dante Peretti <danteperetti34@gmail.com>
2023-03-19 16:51:46 -0400

    Created for loop to create a grid of boxes

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const container = document.getElementById('container');
 function makeRows() {
-    
+    for (let i = 0; i < 256; i++) {
+        const box = document.createElement('div');
+        container.appendChild(box).className = 'box';
+    }
 }
