cffa1f34c03273efe2cc1c0c49fc9b46482be489
Dante Peretti <danteperetti34@gmail.com>
2023-03-19 16:56:31 -0400

    Changed makeRows to makeGrid and added event listener in for loop

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const container = document.getElementById('container');
-function makeRows() {
+function makeGrid() {
     for (let i = 0; i < 256; i++) {
         const box = document.createElement('div');
         container.appendChild(box).className = 'box';
+        box.addEventListener('mouseover', () => 
+            box.style.backgroundColor = 'green');
     }
 }
+makeGrid();
