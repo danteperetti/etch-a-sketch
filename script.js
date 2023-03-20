a82e7eec4a2ea7b75c962f278a90ecf4c017b382
Dante Peretti <danteperetti34@gmail.com>
2023-03-19 20:08:08 -0400

    Created argument for makeGrid and boxCount variable

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const container = document.getElementById('container');
-function makeGrid() {
-    for (let i = 0; i < 256; i++) {
+function makeGrid(size) {
+    const boxCount = size**2;
+    for (let i = 0; i < boxCount; i++) {
         const box = document.createElement('div');
         container.appendChild(box).className = 'box';
         box.addEventListener('mouseover', () => 
             box.style.backgroundColor = 'green');
     }
 }
-makeGrid();
+makeGrid(16);
